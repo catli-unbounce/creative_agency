eb6f0cca70ba41fc2910c43e1adac6f828198cce
catli-unbounce <catherine.li@unbounce.com>
2021-10-15 10:46:34 -0700

    Completed slideshow

---
+++ public/js/script.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+    
+    $('.services__slideshow').hide();
+    $('div[data-slide="1"]').show();
+
+    $('.icon-next, .icon-previous').click(function() {
+        let slide = $(this).data('slide');
+        $('.services__slideshow').hide();
+        $(`div[data-slide="${slide}"]`).show();
+    })
+    
+    $('.menu-icon').click(function() {
+        
+        if($(this).attr('src') === 'images/mobile/icon-hamburger.svg') {
+            $(this).attr('src', 'images/mobile/icon-cross.svg');
+        } else {
+            $(this).attr('src', 'images/mobile/icon-hamburger.svg');
+        }
+
+        $('.navigation__links').slideToggle('slow')
+    })
+
+    $(window).resize(function() {
+        if ($(window).width() > 1000 && $('.menu-icon').attr('src') === 'images/mobile/icon-cross.svg') {
+            $('nav.navigation__links').css("display", "flex");
+        }
+    });
+})
